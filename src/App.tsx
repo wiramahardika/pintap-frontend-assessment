c727daf7ff925f118fd133fb9a3a45e628174598
Wira Mahardika <wiramahardikka@gmail.com>
2022-11-07 08:25:02 +0800

    chore: react-query setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App: FC = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <QueryClientProvider client={queryClient}>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </QueryClientProvider>
   );
 };
 
